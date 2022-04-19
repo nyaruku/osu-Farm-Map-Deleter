e5b0d7ea3bcf0d9b3815e16d36b5129563a0b504
Railgun <38096703+KilluaYT@users.noreply.github.com>
2022-04-19 19:21:08 +0200

    Add files via upload

--- osu!FarmMapsDeleter/osu!FarmMapsDeleter/Form1.Designer.cs
+++ osu!FarmMapsDeleter/osu!FarmMapsDeleter/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace osu_FarmMapsDeleter
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
@@ -41,6 +42,12 @@ namespace osu_FarmMapsDeleter
             this.userControl11 = new SmoothProgressBar.UserControl1();
             this.label4 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -50,9 +57,9 @@ namespace osu_FarmMapsDeleter
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(63, 18);
+            this.label1.Size = new System.Drawing.Size(102, 18);
             this.label1.TabIndex = 0;
-            this.label1.Text = "osu! Dir:";
+            this.label1.Text = "osu! Song Dir:";
             // 
             // textBox1
             // 
@@ -60,11 +67,12 @@ namespace osu_FarmMapsDeleter
             this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.textBox1.ForeColor = System.Drawing.Color.White;
-            this.textBox1.Location = new System.Drawing.Point(82, 10);
+            this.textBox1.Location = new System.Drawing.Point(121, 10);
             this.textBox1.Name = "textBox1";
             this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(416, 24);
+            this.textBox1.Size = new System.Drawing.Size(556, 24);
             this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // label2
             // 
@@ -82,7 +90,7 @@ namespace osu_FarmMapsDeleter
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(13, 94);
+            this.label3.Location = new System.Drawing.Point(12, 249);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(129, 18);
             this.label3.TabIndex = 3;
@@ -93,7 +101,7 @@ namespace osu_FarmMapsDeleter
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(13, 112);
+            this.label5.Location = new System.Drawing.Point(13, 267);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(223, 18);
             this.label5.TabIndex = 5;
@@ -108,9 +116,9 @@ namespace osu_FarmMapsDeleter
             this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.button1.ForeColor = System.Drawing.Color.White;
-            this.button1.Location = new System.Drawing.Point(15, 134);
+            this.button1.Location = new System.Drawing.Point(340, 299);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(483, 36);
+            this.button1.Size = new System.Drawing.Size(337, 36);
             this.button1.TabIndex = 6;
             this.button1.Text = "DELETE ALL FARM";
             this.button1.UseVisualStyleBackColor = false;
@@ -121,18 +129,18 @@ namespace osu_FarmMapsDeleter
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(13, 220);
+            this.label6.Location = new System.Drawing.Point(13, 377);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(158, 18);
             this.label6.TabIndex = 7;
-            this.label6.Text = "(C) Railgun 2021 | v1.0";
+            this.label6.Text = "(C) Railgun 2022 | v1.1";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(340, 220);
+            this.label7.Location = new System.Drawing.Point(520, 377);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(157, 18);
             this.label7.TabIndex = 8;
@@ -147,9 +155,9 @@ namespace osu_FarmMapsDeleter
             this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
             this.button2.ForeColor = System.Drawing.Color.White;
-            this.button2.Location = new System.Drawing.Point(367, 40);
+            this.button2.Location = new System.Drawing.Point(523, 41);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(132, 31);
+            this.button2.Size = new System.Drawing.Size(154, 31);
             this.button2.TabIndex = 9;
             this.button2.Text = "Choose osu! Song Path";
             this.button2.UseVisualStyleBackColor = false;
@@ -158,12 +166,12 @@ namespace osu_FarmMapsDeleter
             // userControl11
             // 
             this.userControl11.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(6)))), ((int)(((byte)(6)))));
-            this.userControl11.Location = new System.Drawing.Point(14, 202);
+            this.userControl11.Location = new System.Drawing.Point(12, 341);
             this.userControl11.Maximum = 100;
             this.userControl11.Minimum = 0;
             this.userControl11.Name = "userControl11";
             this.userControl11.ProgressBarColor = System.Drawing.Color.Blue;
-            this.userControl11.Size = new System.Drawing.Size(483, 15);
+            this.userControl11.Size = new System.Drawing.Size(665, 15);
             this.userControl11.TabIndex = 10;
             this.userControl11.Value = 0;
             // 
@@ -172,7 +180,7 @@ namespace osu_FarmMapsDeleter
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(13, 76);
+            this.label4.Location = new System.Drawing.Point(12, 64);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(122, 18);
             this.label4.TabIndex = 4;
@@ -183,18 +191,97 @@ namespace osu_FarmMapsDeleter
             this.label8.AutoSize = true;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
             this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(12, 181);
+            this.label8.Location = new System.Drawing.Point(13, 359);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(28, 18);
             this.label8.TabIndex = 11;
             this.label8.Text = "----";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Blue;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button3.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.button3.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Navy;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(12, 299);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(322, 36);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "START FILTER";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(6)))), ((int)(((byte)(6)))), ((int)(((byte)(6)))));
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.Color.White;
+            this.textBox2.Location = new System.Drawing.Point(113, 157);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(564, 24);
+            this.textBox2.TabIndex = 13;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(12, 111);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(658, 18);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "Before you filter, put in Milliseconds the maximum Number, till which it isn\'t co" +
+    "nsidered as farm map";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(13, 136);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(459, 18);
+            this.label10.TabIndex = 15;
+            this.label10.Text = "Example: 30000 = (everything under 30sec is farm and will be filtered)";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.label11.ForeColor = System.Drawing.Color.White;
+            this.label11.Location = new System.Drawing.Point(13, 159);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(94, 18);
+            this.label11.TabIndex = 16;
+            this.label11.Text = "Milliseconds:";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F);
+            this.label12.ForeColor = System.Drawing.Color.White;
+            this.label12.Location = new System.Drawing.Point(13, 187);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(378, 13);
+            this.label12.TabIndex = 17;
+            this.label12.Text = "(If nothing put in, everything under 1min 30sec \"90000\" will be marked as farm)";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(16)))), ((int)(((byte)(16)))), ((int)(((byte)(16)))));
-            this.ClientSize = new System.Drawing.Size(510, 247);
+            this.ClientSize = new System.Drawing.Size(689, 404);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.userControl11);
             this.Controls.Add(this.button2);
@@ -208,6 +295,7 @@ namespace osu_FarmMapsDeleter
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.Text = "osu!FarmMapsDeleter";
@@ -233,6 +321,12 @@ namespace osu_FarmMapsDeleter
         private SmoothProgressBar.UserControl1 userControl11;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
     }
 }
 
--- osu!FarmMapsDeleter/osu!FarmMapsDeleter/Form1.cs
+++ osu!FarmMapsDeleter/osu!FarmMapsDeleter/Form1.cs
@@ -1,34 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
+﻿using Microsoft.WindowsAPICodePack.Dialogs;
+using Microsoft.WindowsAPICodePack;
+using Microsoft.WindowsAPICodePack.Shell;
+using System;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using Osu_BeatmapDataReader;
-using OsuSongPathSearcher;
 using System.IO;
 using System.Threading;
-using osu_FarmMapsDeleter;
-using Microsoft.WindowsAPICodePack.Dialogs;
+using System.Windows.Forms;
+using OsuBeatmapReader;
 
 namespace osu_FarmMapsDeleter
 {
-   
     public partial class Form1 : Form
     {
-
-        public ListView lv1 = new ListView();
+        public ListView lv1 = new ListView(); // all maps
         public ListView lv2 = new ListView();
         public ListView farmMaps = new ListView();
         public string osuSongPath;
-     public   decimal totalFarm;
-       OsuSongPathSearcher.Class1 PathSearcher = new OsuSongPathSearcher.Class1();
-        Osu_BeatmapDataReader.BeatmapReader bmReader = new Osu_BeatmapDataReader.BeatmapReader();
+        public decimal totalFarm;
+
       
-        
+        private OsuBeatmapReader.OsuBeatmapReader obm = new OsuBeatmapReader.OsuBeatmapReader();
         public Form1()
         {
             InitializeComponent();
@@ -37,74 +28,65 @@ namespace osu_FarmMapsDeleter
         private void Form1_Load(object sender, EventArgs e)
         {
             CheckForIllegalCrossThreadCalls = false;
-            PathSearcher.GetOsuSongPath();
-            textBox1.Text = PathSearcher.OsuSongPath;
-            osuSongPath = PathSearcher.OsuSongPath;
-            label2.Text = PathSearcher.Status;
+            //    PathSearcher.GetOsuSongPath();
+            // textBox1.Text = PathSearcher.OsuSongPath;
+            //  osuSongPath = PathSearcher.OsuSongPath;
+            //  label2.Text = PathSearcher.Status;
             button1.Enabled = false;
-            if (PathSearcher.Status == "Auto detecting osu! song folder failed.")
-            {
-                button1.Enabled = false;
-            }
-            else
-            {
-                button2.Enabled = false;
-                GetSongs();
-                Thread TH = new Thread(FilterFarmMaps);
-                TH.Start();
-              
-               
-            }
-       
+            button3.Enabled = false;
 
+            // button2.Enabled = true;
+            /*
+                 GetSongs();
+                 Thread TH = new Thread(FilterFarmMaps);
+                 TH.Start();
 
-
-         
-         
-
-
+              */
         }
-        
-           
-        
+
         private void GetSongs()
         {
+            try
+            {
+                string[] filelist;
 
+                filelist = Directory.GetDirectories(osuSongPath);
 
-
-
-                try
+                foreach (string s in filelist)
                 {
-                    string[] filelist;
+                    string[] TempPath;
 
-                    filelist = Directory.GetDirectories(osuSongPath);
+                    TempPath = Directory.GetFiles(s);
+                    lv2.Items.Add(s);
 
-                    foreach (string s in filelist)
+                    foreach (String a in TempPath)
                     {
-                        string[] TempPath;
-
-                        TempPath = Directory.GetFiles(s);
-                        lv2.Items.Add(s);
-
-                        foreach (String a in TempPath)
+                        if (a.Contains(".osu"))
                         {
-                            if (a.Contains(".osu"))
-                            {
-                                lv1.Items.Add(a);
+                            lv1.Items.Add(a);
                             label4.Text = "Total osu! maps: " + lv1.Items.Count;
-                          
-                            }
                         }
-                   
                     }
                 }
-                catch { label2.Text = "Error: Invailid Path" + Environment.NewLine + "(" + osuSongPath + ")"; label6.Visible = true; }
-
-            
+                button3.Enabled = true;
+            }
+            catch { label2.Text = "Error: Invailid Path" + Environment.NewLine + "(" + osuSongPath + ")"; label6.Visible = true; }
         }
 
         private void FilterFarmMaps()
         {
+
+            int MapTime;
+            if (textBox2.Text == "")
+            {
+                MapTime = 90000;
+            }
+            else
+            {
+                MapTime = Convert.ToInt32(textBox2.Text);
+              
+            }
+            
             label2.Text = "Searching for Farm Maps (it can take a while)";
 
             userControl11.Maximum = lv1.Items.Count;
@@ -113,59 +95,47 @@ namespace osu_FarmMapsDeleter
             {
                 try
                 {
-                    bmReader.BeatmapPath = item.Text;
-                
-                bmReader.GetBeatmapData();
-                i++;
-                if (bmReader.Creator.Contains("Sotarks")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Log Off Now")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("browiec")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Reform")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Kuki1537")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("monstrata")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("A r M i N")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Nevo")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("fieryrage")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("DeRandom Otaku")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Akitoshi")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("SnowNiNo_")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Azunyan")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Fatfan Kolek")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Yuuma")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("PikAqours")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Taeyang")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Kowari")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Shmiklak")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                if (bmReader.Creator.Contains("Lami")) { farmMaps.Items.Add(bmReader.BeatmapPath); }
-                label3.Text = "Farm Maps found: " + farmMaps.Items.Count;
-                decimal a = farmMaps.Items.Count;
-                decimal b = lv1.Items.Count;
-                totalFarm = a/b;
-                label5.Text = ((a/b)*100).ToString("0.000") + "% of your osu! maps are Farm";
-                userControl11.Value = i;
-                label8.Text = i + " / " + lv1.Items.Count;
-               
+                 //   bmReader.BeatmapPath = item.Text;
+
+                 //   bmReader.GetBeatmapData();
+                  
+                    i++;
+                    obm.GetBeatmapData(item.Text);
+                    if(obm.MapLength <= MapTime)
+                    {
+                        farmMaps.Items.Add(item.Text);
+                    }
+
+
+
+                    label3.Text = "Farm Maps found: " + farmMaps.Items.Count;
+                    decimal a = farmMaps.Items.Count;
+                    decimal b = lv1.Items.Count;
+                    totalFarm = a / b;
+                    label5.Text = ((a / b) * 100).ToString("0.000") + "% of your osu! maps are Farm";
+                    userControl11.Value = i;
+                    label8.Text = i + " / " + lv1.Items.Count;
                 }
-                catch {  }
+                catch { }
             }
             if (farmMaps.Items.Count == 0)
             {
                 button1.Enabled = false;
+                button3.Enabled = true;
                 label2.Text = "No Farm Maps Found";
             }
             else
             {
                 button1.Enabled = true;
+
                 label2.Text = "You can now click 'DELETE ALL FARM'.";
             }
-            
         }
 
         private void Form1_FormClosing(object sender, FormClosingEventArgs e)
         {
             Application.Exit();
         }
-
 
         public void DeleteAllFarm()
         {
@@ -177,14 +147,10 @@ namespace osu_FarmMapsDeleter
             {
                 try
                 {
-
                     File.Delete(item.Text);
 
                     farmMaps.Items.Remove(item);
 
-                   
-                   
-                  
                     decimal a = farmMaps.Items.Count;
                     decimal b = lv1.Items.Count;
                     totalFarm = a / b;
@@ -197,34 +163,46 @@ namespace osu_FarmMapsDeleter
             }
         }
 
-
         private void button2_Click(object sender, EventArgs e)
         {
             CommonOpenFileDialog dialog = new CommonOpenFileDialog();
-            
+
             dialog.IsFolderPicker = true;
             if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
             {
                 textBox1.Text = dialog.FileName;
                 osuSongPath = textBox1.Text;
                 GetSongs();
-               Thread TH = new Thread(FilterFarmMaps);
-               TH.Start();
-                
 
+                //Thread TH = new Thread(FilterFarmMaps);
+                //TH.Start();
             }
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
             Thread Th2 = new Thread(DeleteAllFarm);
-            
+
             Th2.Start();
         }
 
         private void Form1_FormClosed(object sender, FormClosedEventArgs e)
         {
             Application.Exit();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            osuSongPath = textBox1.Text;
+            button3.Enabled = false;
+
+           // GetSongs();
+            Thread TH = new Thread(FilterFarmMaps);
+            TH.Start();
         }
     }
 }
